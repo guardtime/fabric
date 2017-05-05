559833e8f2bb3dda6a3fbbd732283f028d4232d3
Ryan Elliott <ryan.elliott@guardtime.com>
2017-05-05 09:02:14 -0700

    Save the signature into the CouchDB database used by Fabric so that clients can later look it up and verify a block.

--- core/committer/committer_impl.go
+++ core/committer/committer_impl.go
@@ -24,6 +24,11 @@ import (
 	"github.com/op/go-logging"
 	"github.com/GuardTime/go-ksi"
 	"github.com/kelseyhightower/envconfig"
+	"net/http"
+	"io/ioutil"
+	"strings"
+	"encoding/json"
+	"fmt"
 )
 
 //--------!!!IMPORTANT!!-!!IMPORTANT!!-!!IMPORTANT!!---------
@@ -65,6 +70,65 @@ func initKSIContext() (*ksi.KSI, error) {
 	return signer, nil
 }
 
+func saveSignature( blockHeader *common.BlockHeader, signature []byte) (string, error){
+	dbResponse, err := http.Get("http://couchdb:5984/_all_dbs")
+	if err != nil {
+		logger.Errorf("Failed to list databases: %v\n", err.Error())
+		return "", err
+	}
+	defer dbResponse.Body.Close()
+	dbBody, err := ioutil.ReadAll(dbResponse.Body)
+	logger.Debugf("We got the list of databases: %s",dbBody)
+
+	var client = &http.Client{}
+	if( !strings.Contains(string(dbBody),"signatures") ){
+		logger.Debugf("We need to create the Signature database: ")
+		dbRequest, err := http.NewRequest("PUT", "http://couchdb:5984/signatures", nil)
+		// if err != nil {
+		// 	logger.Errorf("Failed to create PUT: %v\n", err.Error())
+		// 	return "", err
+		// }
+		dbCreateResponse, err := client.Do(dbRequest)
+		if err != nil {
+			logger.Errorf("Failed to create database: %v\n", err.Error())
+			return "", err
+		}
+		dbCreateBody, err := ioutil.ReadAll(dbCreateResponse.Body)
+		// if err != nil {
+		// 	logger.Errorf("Failed to database response: %v\n", err.Error())
+		// 	return "", err
+		// }
+		logger.Debugf("We created the Signature database: %s",dbCreateBody)
+	}
+	dbResponse, err = http.Get("http://couchdb:5984/_uuids")
+	// if err != nil {
+	// 	logger.Errorf("Failed to list databases: %v\n", err.Error())
+	// 	return "", err
+	// }
+	defer dbResponse.Body.Close()
+	responseBytes, err := ioutil.ReadAll(dbResponse.Body)
+	var dat map[string]interface{}
+	if err := json.Unmarshal(responseBytes, &dat); err != nil {
+        logger.Errorf("Failed to get UUID: %v\n", err.Error())
+    }
+    uuids := dat["uuids"].([]interface{})
+    uuid := uuids[0].(string)
+    logger.Debugf("We are going to use the UUID: %s",uuid)
+    
+    logger.Debugf("We need to save the Signature: ")
+    signatureJson := fmt.Sprintf("{\"block\":%d,\"dataHash\":\"%x\",\"signature\":\"%x\"}",blockHeader.Number,blockHeader.DataHash,signature)
+	logger.Debugf("We need to save the Signature: %s",signatureJson)
+    dbRequest, err := http.NewRequest("PUT", "http://couchdb:5984/signatures/"+uuid, strings.NewReader(signatureJson))
+	dbCreateResponse, err := client.Do(dbRequest)
+	// if err != nil {
+	// 	logger.Errorf("Failed to create database: %v\n", err.Error())
+	// 	return "", err
+	// }
+	dbCreateBody, err := ioutil.ReadAll(dbCreateResponse.Body)
+	logger.Debugf("We saved the Signature in the database: %s",dbCreateBody)
+	return uuid, nil;
+}
+
 // Commit commits block to into the ledger
 // Note, it is important that this always be called serially
 func (lc *LedgerCommitter) Commit(block *common.Block) error {
@@ -91,9 +155,12 @@ func (lc *LedgerCommitter) Commit(block *common.Block) error {
 		} else {
 			logger.Debugf("Adding the KSI Signature to the Block metadata.")
 			signatureBytes, err := sig.Bytes()
+
 			if err != nil {
 				logger.Errorf("Failed to serialize the KSI Signature to raw bytes. Error: %v\n", err.Error())
 			} else {
+				saveSignature( block.Header, signatureBytes );
+				
 				block.Metadata.Metadata = append(block.Metadata.Metadata, signatureBytes);
 				logger.Debugf("Now, the metadata array has %d elements",len(block.Metadata.Metadata))
 			}
